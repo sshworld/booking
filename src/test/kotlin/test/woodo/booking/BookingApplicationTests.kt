651b3dbf3e282b691f6c90565f053bc4b4aee0a3
sshworld <59462537+sshworld@users.noreply.github.com>
2024-02-24 21:57:15 +0900

    init : 프로젝트 생성

---
+++ src/test/kotlin/test/woodo/booking/BookingApplicationTests.kt
@@ -0,0 +1,13 @@
+package test.woodo.booking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
