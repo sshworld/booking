2d5c750f5f805d8dd95db6fdd6a28f46d11ed268
sshworld <59462537+sshworld@users.noreply.github.com>
2024-02-27 20:13:11 +0900

    chore: import 제거

--- src/main/kotlin/test/woodo/booking/consignment/controller/ConsignmentController.kt
+++ src/main/kotlin/test/woodo/booking/consignment/controller/ConsignmentController.kt
@@ -4,13 +4,11 @@ import java.security.Principal
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.OK
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import test.woodo.booking.consignment.controller.data.ConsignmentCreateRequest
