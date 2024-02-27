0617aabcd664d6f115b3554e24f9e52b40f2f8b9
sshworld <59462537+sshworld@users.noreply.github.com>
2024-02-28 01:53:01 +0900

    remove: 사용하지 않는 response 제거

--- src/main/kotlin/test/woodo/booking/rental/controller/RentalController.kt
+++ src/main/kotlin/test/woodo/booking/rental/controller/RentalController.kt
@@ -1,11 +1,9 @@
 package test.woodo.booking.rental.controller
 
 import java.security.Principal
-import org.springframework.http.HttpStatus.CREATED
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import test.woodo.booking.rental.controller.data.RentalRequest
 import test.woodo.booking.rental.service.RentalService
@@ -16,7 +14,6 @@ class RentalController(
     private val rentalService: RentalService,
 ) {
     @PostMapping
-    @ResponseStatus(CREATED)
     fun rental(
         principal: Principal,
         @RequestBody request: RentalRequest,
--- src/main/kotlin/test/woodo/booking/rental/controller/data/RentalResponse.kt
+++
@@ -1,9 +0,0 @@
-package test.woodo.booking.rental.controller.data
-
-import test.woodo.booking.rental.domain.Rental
-
-class RentalResponse(
-
-) {
-    constructor(rental: Rental) : this()
-}
