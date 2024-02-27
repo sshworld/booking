b21a75071d14e361e35cf00a9cbcf33adfc00ed1
sshworld <59462537+sshworld@users.noreply.github.com>
2024-02-28 06:54:06 +0900

    chore: test dependency 구성

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,10 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.6.1")
+    testImplementation("io.kotest:kotest-property-jvm:5.6.1")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.6.1")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
