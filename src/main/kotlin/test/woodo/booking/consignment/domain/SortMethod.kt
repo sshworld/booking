fb87d70dff690c8c45589a386efe0979e3388bc9
sshworld <59462537+sshworld@users.noreply.github.com>
2024-02-27 20:06:39 +0900

    fix: 정렬 시 properties 변수명 적용

--- src/main/kotlin/test/woodo/booking/consignment/domain/SortMethod.kt
+++ src/main/kotlin/test/woodo/booking/consignment/domain/SortMethod.kt
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Sort
 enum class SortMethod(val sort: Sort) {
     MANY(Sort.by("rentals").descending()),
 
-    LOW_PRICE(Sort.by("price").ascending()),
+    LOW_PRICE(Sort.by("rentalPrice").ascending()),
 
-    RECENT(Sort.by("created_at").descending()),
+    RECENT(Sort.by("createdAt").descending()),
 }
