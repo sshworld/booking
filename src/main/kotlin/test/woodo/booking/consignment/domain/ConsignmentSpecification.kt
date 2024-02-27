2d4fceb7459ec867929d3965d2497740baf81322
sshworld <59462537+sshworld@users.noreply.github.com>
2024-02-28 03:25:44 +0900

    fix: 위탁 도서 전체 조회 누락 수정

--- src/main/kotlin/test/woodo/booking/consignment/domain/ConsignmentSpecification.kt
+++ src/main/kotlin/test/woodo/booking/consignment/domain/ConsignmentSpecification.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.domain.Specification
 object ConsignmentSpecification {
     fun of(status: Status?): Specification<Consignment> {
         return Specification { root, _, criteriaBuilder ->
-            criteriaBuilder.equal(root.get<Consignment>("status"), status)
+            criteriaBuilder.equal(root.get<Consignment>("status"), status).takeIf { status != null }
         }
     }
 }
